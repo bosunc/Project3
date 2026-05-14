84e192a39a91844fdfa9b97240313e556e1499dc
SKS_hanryang <bosun1985@gmail.com>
2026-05-14 10:31:09 +0900

    memory

--- Project3/newman.cpp
+++ Project3/newman.cpp
@@ -1,37 +1,19 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-// 구조체 정의
-struct Player {
-    string name;
-    string position;
-    int height;
-    int weight;
-};
+void changeValue(int* p) {
+    *p = 100;
+}
 
 int main() {
 
-    // 구조체 배열 생성
-    Player team[3] = {
-        {"Son", "Forward", 183, 77},
-        {"Kim", "Defender", 190, 82},
-        {"Lee", "Midfielder", 175, 68}
-    };
+    int num = 10;
 
-    // 배열 출력
-    for (int i = 0; i < 3; i++) {
+    cout << "변경 전 : " << num << endl;
 
-        cout << "===== Player " << i + 1 << " =====" << endl;
+    changeValue(&num);
 
-        cout << "Name : " << team[i].name << endl;
-        cout << "Position : " << team[i].position << endl;
-        cout << "Height : " << team[i].height << endl;
-        cout << "Weight : " << team[i].weight << endl;
-
-        cout << endl;
-    }
+    cout << "변경 후 : " << num << endl;
 
     return 0;
 }
