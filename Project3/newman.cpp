8aa90431176e3b9ae31eb701266d2fbff063f10e
SKS_hanryang <bosun1985@gmail.com>
2026-05-14 10:30:26 +0900

    commit 2

--- Project3/newman.cpp
+++ Project3/newman.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
+// 구조체 정의
+struct Player {
+    string name;
+    string position;
+    int height;
+    int weight;
+};
+
 int main() {
-    int a, b;
 
-    for (int i = 0; i < 5; i++) {
-        cin >> a >> b;
+    // 구조체 배열 생성
+    Player team[3] = {
+        {"Son", "Forward", 183, 77},
+        {"Kim", "Defender", 190, 82},
+        {"Lee", "Midfielder", 175, 68}
+    };
 
-        if (a > b)
-            cout << a << endl;
-        else
-            cout << b << endl;
+    // 배열 출력
+    for (int i = 0; i < 3; i++) {
+
+        cout << "===== Player " << i + 1 << " =====" << endl;
+
+        cout << "Name : " << team[i].name << endl;
+        cout << "Position : " << team[i].position << endl;
+        cout << "Height : " << team[i].height << endl;
+        cout << "Weight : " << team[i].weight << endl;
+
+        cout << endl;
     }
 
     return 0;
